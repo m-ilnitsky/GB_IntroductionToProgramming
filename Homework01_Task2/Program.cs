c2eaa97e27da0ed741924f74bcbe9dd378afb667
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-05-24 01:56:58 +0700

    Added Homework01

---
+++ Homework01_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+
+//int maxNumber = number1 > number2 ? number1 : number2;
+//int maxNumber = Math.Max(number1, number2);
+int maxNumber;
+
+if (number1 > number2)
+{
+    maxNumber = number1;
+}
+else
+{
+    maxNumber = number2;
+}
+
+Console.WriteLine("Максимальное число: " + maxNumber);
