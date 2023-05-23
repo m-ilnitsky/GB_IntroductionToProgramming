0d8e90907dc80b5d62999aac057c4434fb437189
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-05-24 00:27:19 +0700

    Added Seminar01_Task7

---
+++ Seminar01_Task7/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int lastDigit = number % 10;
+System.Console.WriteLine($"Последняя цифра числа {number} это цифра {lastDigit}");
