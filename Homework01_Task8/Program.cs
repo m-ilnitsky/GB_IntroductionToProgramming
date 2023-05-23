60f1819bf66645b0878532708e87f6aae97cdbed
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-05-24 02:39:14 +0700

    Added Homework01_Task8

---
+++ Homework01_Task8/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number < 2)
+{
+    Console.WriteLine($"Число {number} меньше минимального четного положительного числа");
+    return;
+}
+
+Console.WriteLine($"Чётные числа от 1 до {number}:");
+
+// Вариант 1
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + "  ");
+    }
+}
+
+Console.WriteLine();
+
+// Вариант 2
+for (int i = 2; i <= number; i += 2)
+{
+    Console.Write(i + "  ");
+}
+
+Console.WriteLine();
+
+// Вариант 3
+int j = 1;
+while (j <= number)
+{
+    if (j % 2 == 0)
+    {
+        Console.Write(j + "  ");
+    }
+
+    j++;
+}
+
+Console.WriteLine();
+
+// Вариант 4
+j = 1;
+do
+{
+    if (j % 2 == 0)
+    {
+        Console.Write(j + "  ");
+    }
+
+    j++;
+}
+while (j <= number);
+
+Console.WriteLine();
