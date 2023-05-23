0da26cc64f0be67ebcc63dcf8919cd2b427cf2e8
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-05-24 02:08:57 +0700

    Added Homework01_Task4

---
+++ Homework01_Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine() ?? "0");
+
+int maxNumber = number1;
+
+if (maxNumber < number2)
+{
+    maxNumber = number2;
+}
+
+if (maxNumber < number3)
+{
+    maxNumber = number3;
+}
+
+Console.WriteLine("Максимальное число: " + maxNumber);
