ed72d82294bfceeabd58b97b4f4b7d56ee8107a2
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-05-23 23:45:19 +0700

    Added Seminar01_Task1

--- Seminar01_Task1/Program.cs
+++ Seminar01_Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+
+if (number1 * number1 == number2)
+{
+    Console.WriteLine($"Число {number2} является квадратом числа {number1}");
+}
+else if (number2 * number2 == number1)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Числа {number2} и {number1} НЕ являются квадратами друг друга");
+}
