637f2467e921f21d9942d7954b2bce63881f0e1f
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-05-24 00:02:42 +0700

    Added Seminar01_Task3

---
+++ Seminar01_Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("Введите номер дня: ");
+int dayNumber = int.Parse(Console.ReadLine() ?? "0");
+
+System.Console.WriteLine();
+Console.WriteLine($"Введено число {dayNumber}");
+
+if (dayNumber < 1)
+{
+    Console.WriteLine("Номер дня недели должен быть >= 1");
+}
+else if (dayNumber > 7)
+{
+    Console.WriteLine("Номер дня недели должен быть <= 7");
+}
+else if (dayNumber == 1)
+{
+    Console.WriteLine("День недели - понедельник");
+}
+else if (dayNumber == 2)
+{
+    Console.WriteLine("День недели - вторник");
+}
+else if (dayNumber == 3)
+{
+    Console.WriteLine("День недели - среда");
+}
+else if (dayNumber == 4)
+{
+    Console.WriteLine("День недели - четверг");
+}
+else if (dayNumber == 5)
+{
+    Console.WriteLine("День недели - пятница");
+}
+else if (dayNumber == 6)
+{
+    Console.WriteLine("День недели - суббота");
+}
+else
+{
+    Console.WriteLine("День недели - воскресенье");
+}
+
+
