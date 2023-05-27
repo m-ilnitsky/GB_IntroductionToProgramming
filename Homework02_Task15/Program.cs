0a8ffe1de8f8d3af64d586470b70a0135327c702
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-05-28 00:25:12 +0700

    Added Homework02_Task15

---
+++ Homework02_Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿bool isValidDayNumber = false;
+int dayNumber;
+
+do
+{
+    Console.Write("Введите номер дня недели (число от 1 до 7): ");
+    dayNumber = int.Parse(Console.ReadLine() ?? "0");
+
+    if (dayNumber < 1)
+    {
+        Console.WriteLine($"Введённое число {dayNumber} меньше 1");
+    }
+    else if (dayNumber > 7)
+    {
+        Console.WriteLine($"Введённое число {dayNumber} больше 7");
+    }
+    else
+    {
+        isValidDayNumber = true;
+    }
+}
+while (!isValidDayNumber);
+
+if (dayNumber > 5)
+{
+    Console.WriteLine($"{dayNumber}й день недели является выходным");
+}
+else
+{
+    Console.WriteLine($"{dayNumber}й день недели является будним днём");
+}
