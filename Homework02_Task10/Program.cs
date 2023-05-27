dd7848a9eb4980f1e172192014cc96d734b0f6e7
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-05-27 23:53:41 +0700

    Added Homework02_Task10

---
+++ Homework02_Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите  число от 100 до 999: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int absNumber = Math.Abs(number);
+Console.WriteLine($"Выбрано число: {absNumber}");
+
+if (absNumber < 10)
+{
+    Console.WriteLine("Невозможно вывести вторую цифру числа, состоящего из одной цифры!");
+    return;
+}
+
+int secondDigit = (absNumber % 100) / 10;
+Console.WriteLine($"Вторая цифра числа {absNumber} это: {secondDigit}");
