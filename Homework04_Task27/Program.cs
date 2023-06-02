42df43c1df28bb3624c9c5b5bb0ac1b26c3fb19f
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-06-02 12:17:59 +0700

    Added Homework04_Task27

---
+++ Homework04_Task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в чисел.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int positiveNumber = Math.Abs(number);
+
+int digitsCount = 0;
+int digitsSum = 0;
+
+do
+{
+    int digit = positiveNumber % 10;
+    positiveNumber /= 10;
+
+    digitsCount++;
+    digitsSum += digit;
+}
+while (positiveNumber > 0);
+
+Console.WriteLine($"Число {number} содержит {digitsCount} цифр. Сумма цифр равна: {digitsSum}");
