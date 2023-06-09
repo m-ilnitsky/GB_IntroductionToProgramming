22f74339095798268d199d50aa2d03d7cca9fd76
Mike_Vys-11 <mike-nsk@ngs.ru>
2023-06-10 01:13:12 +0700

    Added Homework05_Task36

---
+++ Homework05_Task36/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetRandomNumbers(int numbersCount, int minNumber, int maxNumber)
+{
+    var rnd = new Random();
+    var numbers = new int[numbersCount];
+    var rightBorder = maxNumber + 1;
+
+    for (int i = 0; i < numbersCount; i++)
+    {
+        numbers[i] = rnd.Next(minNumber, rightBorder);
+    }
+
+    return numbers;
+}
+
+void WriteNumbers(int[] numbers)
+{
+    Console.Write($"[{string.Join(", ", numbers)}]");
+}
+
+void WriteNumbersLine(int[] numbers)
+{
+    WriteNumbers(numbers);
+    Console.WriteLine();
+}
+
+int GetSumOfNumbersWithOddIndex(int[] numbers)
+{
+    int sumOfNumbersWithOddIndex = 0;
+
+    for (int i = 1; i < numbers.Length; i += 2)
+    {
+        sumOfNumbersWithOddIndex += numbers[i];
+    }
+
+    return sumOfNumbersWithOddIndex;
+}
+
+var numbers = GetRandomNumbers(10, 1, 10);
+
+Console.Write("Массив: ");
+WriteNumbersLine(numbers);
+
+Console.WriteLine($"Сумма чисел на нечётных позициях: {GetSumOfNumbersWithOddIndex(numbers)}");
